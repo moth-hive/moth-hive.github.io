ed679f32d8c000825ca91fb867d456ff8f5a9950
Solar-Moth <thaddeus.cobb@sjsu.edu>
2024-11-11 21:56:33 -0800

    disabled controls for real this time

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
 import * as THREE from 'three';
 
 // Import add-ons
-import { OrbitControls } from 'https://unpkg.com/three@0.162.0/examples/jsm/controls/OrbitControls.js';
+//import { OrbitControls } from 'https://unpkg.com/three@0.162.0/examples/jsm/controls/OrbitControls.js';
 //import { GLTFLoader } from 'https://unpkg.com/three@0.162.0/examples/jsm/loaders/GLTFLoader.js'; // to load 3d models
 
 let scene, camera, renderer, loader;
